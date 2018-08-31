acb37fccc62344ac520cf566a375b03aab68998a
solotaker <1327270611@qq.com>
2018-08-31 16:22:18 +0800

    add battery power read example

--- esp32/libraries/ESP32/examples/ADC_Read_Voltage/ADC_Read_Accurate/ADC_Read_Accurate.ino
+++ esp32/libraries/ESP32/examples/ADC_Read_Voltage/ADC_Read_Accurate/ADC_Read_Accurate.ino
@@ -19,7 +19,7 @@ void loop() {
 double ReadVoltage(byte pin){
   double reading = analogRead(pin); // Reference voltage is 3v3 so maximum reading is 3v3 = 4095 in range 0 to 4095
   if(reading < 1 || reading >= 4095)
-    return 0;
+    //return 0;
   // return -0.000000000009824 * pow(reading,3) + 0.000000016557283 * pow(reading,2) + 0.000854596860691 * reading + 0.065440348345433;
   return -0.000000000000016 * pow(reading,4) + 0.000000000118171 * pow(reading,3)- 0.000000301211691 * pow(reading,2)+ 0.001109019271794 * reading + 0.034143524634089;
 } // Added an improved polynomial, use either, comment out as required
