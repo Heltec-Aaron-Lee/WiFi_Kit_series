405af26e46c6888f16d7a4ac2984ac77deb045d3
Heltec-Aaron-Lee <support@heltec.cn>
2018-11-06 17:27:54 +0800

    Fix LED defintion

--- esp32/libraries/WiFi/examples/WiFiAccessPoint/WiFiAccessPoint.ino
+++ esp32/libraries/WiFi/examples/WiFiAccessPoint/WiFiAccessPoint.ino
@@ -15,7 +15,7 @@
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 
-#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#define LED_BUILTIN 25   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
 
 // Set these to your desired credentials.
 const char *ssid = "yourAP";
