6f4911471b275f911d56481b69441643b8315fff
Heltec-Aaron-Lee <support@heltec.cn>
2021-01-27 12:31:05 +0800

    Fix chip ID read example

--- esp32/libraries/ESP32/examples/ChipID/GetChipID/GetChipID.ino
+++ esp32/libraries/ESP32/examples/ChipID/GetChipID/GetChipID.ino
@@ -10,20 +10,21 @@ an identifier that can be no more than a 32-bit integer
 created 2020-06-07 by cweinhofer
 with help from Cicicok */
 	
-uint32_t chipId = 0;
+uint64_t chipId = 0;
 
 void setup() {
 	Serial.begin(115200);
 }
 
 void loop() {
-	for(int i=0; i<17; i=i+8) {
-	  chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
-	}
+//	  chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+  chipId=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+  Serial.printf("ESP32ChipID=%04X",(uint16_t)(chipId>>32));//print High 2 bytes
+  Serial.printf("%08X\n",(uint32_t)chipId);//print Low 4bytes.
 
-	Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
-	Serial.printf("This chip has %d cores\n", ESP.getChipCores());
-  Serial.print("Chip ID: "); Serial.println(chipId);
+//	Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
+//	Serial.printf("This chip has %d cores\n", ESP.getChipCores());
+//  Serial.print("Chip ID: "); Serial.println(chipId);
   
 	delay(3000);
 
