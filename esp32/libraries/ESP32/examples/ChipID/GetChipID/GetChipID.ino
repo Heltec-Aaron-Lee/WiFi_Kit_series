946efae7af0fde9b2888d8e99127860d421f2d02
Quency-D <55523105+Quency-D@users.noreply.github.com>
2022-09-30 09:41:29 +0800

    Update GetChipID.ino

--- esp32/libraries/ESP32/examples/ChipID/GetChipID/GetChipID.ino
+++ esp32/libraries/ESP32/examples/ChipID/GetChipID/GetChipID.ino
@@ -1,7 +1,4 @@
-/* The true ESP32 chip ID is essentially its MAC address.
-
-created 2020-06-07 by cweinhofer
-with help from Cicicok */
+/* The true ESP32 chip ID is essentially its MAC address.*/
   
 uint64_t chipId = 0;
 
@@ -13,7 +10,7 @@ void loop() {
   chipId=ESP.getEfuseMac();
   Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
   Serial.printf("This chip has %d cores\n", ESP.getChipCores());
-  Serial.print("Chip ID: "); Serial.println(chipId);
-  
+  Serial.printf("ESP32ChipID=%04X",(uint16_t)(chipId>>32));//print High 2bytes
+  Serial.printf("%08X\r\n",(uint32_t)chipId);//print Low 4bytes.
   delay(3000);
 }
