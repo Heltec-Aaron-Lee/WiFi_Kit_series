792c4b54f53272fd5f7db59476bb26925c62abd8
Bill Shannon <washannon@gmail.com>
2023-03-04 12:32:56 -0500

    Updated to latest package_esp32_index.template.json from espressif, used get.py to update to latest packages.

---
+++ esp32/tools/openocd-esp32/share/openocd/scripts/cpu/arm/arm966.tcl
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set CPU_TYPE   arm
+set CPU_NAME   arm966
+set CPU_ARCH   armv5te
+set CPU_MAX_ADDRESS 0xFFFFFFFF
+set CPU_NBITS  32
---
+++ esp32/tools/openocd-esp32/share/openocd/scripts/cpu/arm/cortex_m3.tcl
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set CPU_TYPE   arm
+set CPU_NAME   cortex_m3
+set CPU_ARCH   armv7
+set CPU_MAX_ADDRESS 0xFFFFFFFF
+set CPU_NBITS  32
