3eac4667aabbb8427272c97a5909f37cf3e92faa
wangwuser <2715534893.qq.com>
2024-01-10 11:30:20 +0800

    Added LOAdded code for Lorawan to send GPS informationGPS

--- libraries/HeltecExample/examples/LoRaWAN/LoRaWanGpsGetTime/LoRaWanGpsGetTime.ino
+++ libraries/HeltecExample/examples/LoRaWAN/LoRaWanGpsGetTime/LoRaWanGpsGetTime.ino
@@ -8,32 +8,33 @@ HT_st7735 st7735;
 
 #define VGNSS_CTRL 3
 /* OTAA para*/
-uint8_t devEui[] = {0x22, 0x32, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t devEui[] = { 0x22, 0x32, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00 };
 uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-uint8_t appKey[] = { 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88};
+uint8_t appKey[] = { 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88 };
 /* ABP para*/
 uint8_t nwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda, 0x85 };
 uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef, 0x67 };
-uint32_t devAddr =  ( uint32_t )0x007e6ae1;
+uint32_t devAddr = (uint32_t)0x007e6ae1;
 
 /*LoraWan channelsmask, default channels 0-7*/
 uint16_t userChannelsMask[6] = { 0x00FF, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
-uint32_t license[4] = {0x21701168, 0xAAAEEC0A, 0xFDA9F9D2, 0x0110E29E};
+uint32_t license[4] = { 0x21701168, 0xAAAEEC0A, 0xFDA9F9D2, 0x0110E29E };
 /*LoraWan region, select in arduino IDE tools*/
 LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
 
-/*LoraWan ClaSerial1, ClaSerial1 A and ClaSerial1 C are supported*/
-DeviceClass_t  loraWanClass = CLASS_A;
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t loraWanClass = CLASS_A;
 
-/*the application data transmiSerial1ion duty cycle.  value in [ms].*/
+/*the application data transmission duty cycle.  value in [ms].*/
 uint32_t appTxDutyCycle = 15000;
 
 /*OTAA or ABP*/
 bool overTheAirActivation = true;
 
 /*ADR enable*/
-bool loraWanAdr = 0;
-/* Indicates if the node is sending confirmed or unconfirmed meSerial1ages */
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
 bool isTxConfirmed = true;
 
 /* Application port */
@@ -44,7 +45,7 @@ uint8_t appPort = 2;
   according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
   to the following table:
 
-  TransmiSerial1ion nb | Data Rate
+  Transmission nb | Data Rate
   ----------------|-----------
   1 (first)       | DR
   2               | DR
@@ -59,76 +60,75 @@ uint8_t appPort = 2;
   the datarate, in case the LoRaMAC layer did not receive an acknowledgment
 */
 uint8_t confirmedNbTrials = 4;
-void GPS_test(void)
-{
+void GPS_test(void) {
 
 
   Serial.println("GPS_test");
   st7735.st7735_fill_screen(ST7735_BLACK);
   delay(100);
-  st7735.st7735_write_str(0, 0, (String)"GPS_test");
+  st7735.st7735_write_str(0, 0, (String) "GPS_test");
 
- 
-    if (Serial1.available() > 0)
-    {
-      if (Serial1.peek() != '\n')
-      {
-        GPS.encode(Serial1.read());
-      }
-      else
-      {
-        Serial1.read();
-     
-      }
+
+  if (Serial1.available() > 0) {
+    if (Serial1.peek() != '\n') {
+      GPS.encode(Serial1.read());
+    } else {
+      Serial1.read();
     }
   }
+}
 
 /* Prepares the payload of the frame */
-static void prepareTxFrame( uint8_t port )
-{
-  /*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commiSerial1ioning.h".
+static void prepareTxFrame(uint8_t port) {
+  /*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
     appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
     if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
     if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
     for example, if use REGION_CN470,
     the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
   */
-
+  // pinMode(VGNSS_CTRL, OUTPUT);
+  // digitalWrite(VGNSS_CTRL, HIGH);
   // pinMode(GPIO0, OUTPUT);
   pinMode(Vext, OUTPUT);
   // digitalWrite(GPIO0, HIGH);
   digitalWrite(Vext, HIGH);
 
-  float lat, lon, alt, course, speed, hdop, sats;
+  uint8_t hour, second, minute, centisecond;
 
-  Serial.println("Waiting for GPS FIX ...");
- 
-  while (!GPS.location.isValid()) 
-  {
+  Serial.println("Waiting for GPS time FIX ...");
+
+  while (!GPS.location.isValid()) {
     //smartDelay(1000);
     uint32_t start = millis();
-    do 
-    {
-      if (Serial1.available()) 
-      {
+    do {
+      if (Serial1.available()) {
         GPS.encode(Serial1.read());
       }
-     } while(GPS.charsProcessed() < 10); //((millis() + start) <5000); 
+    } while (GPS.charsProcessed() < 100);
+    ;  //(GPS.charsProcessed() < 10); //
 
-    if ((millis() + start) > 5000)// && GPS.charsProcessed() < 10) 
+    if ((millis() + start) > 10000)  // && GPS.charsProceSerial1ed() < 10)
     {
-     Serial.println("No GPS data received: check wiring");
-     break;
+      Serial.println("No GPS data received: check wiring");
+      break;
     }
   }
+  Serial.printf(" %02d:%02d:%02d.%02d", GPS.time.hour(), GPS.time.minute(), GPS.time.second(), GPS.time.centisecond());
 
-  lat = GPS.location.lat();
-  lon = GPS.location.lng();
-  alt = GPS.altitude.meters();
-  course = GPS.course.deg();
-  speed = GPS.speed.kmph();
-  sats = GPS.satellites.value();
-  hdop = GPS.hdop.hdop();
+  String time_str = (String)GPS.time.hour() + ":" + (String)GPS.time.minute() + ":" + (String)GPS.time.second() + ":" + (String)GPS.time.centisecond();
+
+  hour = GPS.time.hour();
+  minute = GPS.time.minute();
+  second = GPS.time.second();
+  centisecond = GPS.time.centisecond();
+  // lat = GPS.location.lat();
+  // lon = GPS.location.lng();
+  // alt = GPS.altitude.meters();
+  // course = GPS.course.deg();
+  // speed = GPS.speed.kmph();
+  // sats = GPS.satellites.value();
+  // hdop = GPS.hdop.hdop();
 
   digitalWrite(Vext, LOW);
   // digitalWrite(GPIO0, LOW);
@@ -139,89 +139,83 @@ static void prepareTxFrame( uint8_t port )
   unsigned char *puc;
 
   appDataSize = 0;
-  puc = (unsigned char *)(&lat);
+  puc = (unsigned char *)(&hour);
   appData[appDataSize++] = puc[0];
   appData[appDataSize++] = puc[1];
-  appData[appDataSize++] = puc[2];
-  appData[appDataSize++] = puc[3];
-  puc = (unsigned char *)(&lon);
+  // appData[appDataSize++] = puc[2];
+  // appData[appDataSize++] = puc[3];
+  puc = (unsigned char *)(&minute);
   appData[appDataSize++] = puc[0];
   appData[appDataSize++] = puc[1];
-  appData[appDataSize++] = puc[2];
-  appData[appDataSize++] = puc[3];
-  puc = (unsigned char *)(&alt);
+  // appData[appDataSize++] = puc[2];
+  // appData[appDataSize++] = puc[3];
+  puc = (unsigned char *)(&second);
   appData[appDataSize++] = puc[0];
   appData[appDataSize++] = puc[1];
-  appData[appDataSize++] = puc[2];
-  appData[appDataSize++] = puc[3];
-  puc = (unsigned char *)(&course);
+  // appData[appDataSize++] = puc[2];
+  // appData[appDataSize++] = puc[3];
+  puc = (unsigned char *)(&centisecond);
   appData[appDataSize++] = puc[0];
   appData[appDataSize++] = puc[1];
-  appData[appDataSize++] = puc[2];
-  appData[appDataSize++] = puc[3];
-  puc = (unsigned char *)(&speed);
-  appData[appDataSize++] = puc[0];
-  appData[appDataSize++] = puc[1];
-  appData[appDataSize++] = puc[2];
-  appData[appDataSize++] = puc[3];
-  puc = (unsigned char *)(&hdop);
-  appData[appDataSize++] = puc[0];
-  appData[appDataSize++] = puc[1];
-  appData[appDataSize++] = puc[2];
-  appData[appDataSize++] = puc[3];
-  appData[appDataSize++] = 0x00;
-  appData[appDataSize++] = 0x03;
+  // appData[appDataSize++] = puc[2];
+  // appData[appDataSize++] = puc[3];
+  // puc = (unsigned char *)(&speed);
+  // appData[appDataSize++] = puc[0];
+  // appData[appDataSize++] = puc[1];
+  // appData[appDataSize++] = puc[2];
+  // appData[appDataSize++] = puc[3];
+  // puc = (unsigned char *)(&hdop);
+  // appData[appDataSize++] = puc[0];
+  // appData[appDataSize++] = puc[1];
+  // appData[appDataSize++] = puc[2];
+  // appData[appDataSize++] = puc[3];
+  // appData[appDataSize++] = 0x00;
+  // appData[appDataSize++] = 0x03;
 
-  Serial.print("SATS: ");
-  Serial.print(GPS.satellites.value());
-  Serial.print(", HDOP: ");
-  Serial.print(GPS.hdop.hdop());
-  Serial.print(", LAT: ");
-  Serial.print(GPS.location.lat());
-  Serial.print(", LON: ");
-  Serial.print(GPS.location.lng());
-  Serial.print(", AGE: ");
-  Serial.print(GPS.location.age());
-  Serial.print(", ALT: ");
-  Serial.print(GPS.altitude.meters());
-  Serial.print(", COURSE: ");
-  Serial.print(GPS.course.deg());
-  Serial.print(", SPEED: ");
-  Serial.println(GPS.speed.kmph());
+  // Serial.print("SATS: ");
+  // Serial.print(GPS.satellites.value());
+  // Serial.print(", HDOP: ");
+  // Serial.print(GPS.hdop.hdop());
+  // Serial.print(", LAT: ");
+  // Serial.print(GPS.location.lat());
+  // Serial.print(", LON: ");
+  // Serial.print(GPS.location.lng());
+  // Serial.print(", AGE: ");
+  // Serial.print(GPS.location.age());
+  // Serial.print(", ALT: ");
+  // Serial.print(GPS.altitude.meters());
+  // Serial.print(", COURSE: ");
+  // Serial.print(GPS.course.deg());
+  // Serial.print(", SPEED: ");
+  // Serial.println(GPS.speed.kmph());
   // Serial.print(" BatteryVoltage:");
   // Serial.println(batteryVoltage);
 }
 
 
-
-
-void setup()
-{
+void setup() {
   pinMode(VGNSS_CTRL, OUTPUT);
   digitalWrite(VGNSS_CTRL, HIGH);
   Serial1.begin(115200, SERIAL_8N1, 33, 34);
-   Serial.begin(115200);
+  Serial.begin(115200);
+
   pinMode(Vext, OUTPUT);
   digitalWrite(Vext, HIGH);
   Mcu.setlicense(license);
- st7735.st7735_init();
- st7735.st7735_fill_screen(ST7735_BLACK);
+  st7735.st7735_init();
+  st7735.st7735_fill_screen(ST7735_BLACK);
 
   Mcu.begin();
-  // LoRaWanClass::setDataRateForNoADR(0)
 
   deviceState = DEVICE_STATE_INIT;
 }
-void loop()
-{
-  switch ( deviceState )
-  {
+void loop() {
+  switch (deviceState) {
     case DEVICE_STATE_INIT:
       {
-#if(LORAWAN_DEVEUI_AUTO)
+#if (LORAWAN_DEVEUI_AUTO)
         LoRaWAN.generateDeveuiByChipID();
 #endif
-LoRaWAN.setDataRateForNoADR(3);
 
         LoRaWAN.init(loraWanClass, loraWanRegion);
         break;
@@ -229,25 +223,21 @@ LoRaWAN.setDataRateForNoADR(3);
     case DEVICE_STATE_JOIN:
       {
         LoRaWAN.join();
-		st7735.st7735_write_str(0, 0, "join>>>");
+        st7735.st7735_write_str(0, 0, "join>>>");
 
         break;
       }
     case DEVICE_STATE_SEND:
       {
-        prepareTxFrame( appPort );
-        		st7735.st7735_write_str(0, 0, "send>>>");
-
-        LoRaWAN.send();       
-         		st7735.st7735_write_str(0, 0, "send>>>    ok");
-
+        prepareTxFrame(appPort);
+        LoRaWAN.send();
 
         deviceState = DEVICE_STATE_CYCLE;
         break;
       }
     case DEVICE_STATE_CYCLE:
       {
-        txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+        txDutyCycleTime = appTxDutyCycle + randr(-APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND);
         LoRaWAN.cycle(txDutyCycleTime);
         deviceState = DEVICE_STATE_SLEEP;
         break;
@@ -265,5 +255,4 @@ LoRaWAN.setDataRateForNoADR(3);
         break;
       }
   }
-
 }
