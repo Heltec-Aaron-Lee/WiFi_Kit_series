002aab4f5e0b473b86f27cefb46657571363706a
wangwuser <2715534893.qq.com>
2024-01-12 14:06:54 +0800

    Add wireless_ Paper v1.1 Factory Test Code

--- libraries/HeltecExample/examples/FactoryTest/Wireless_Paper_V1.1_FactoryTest/Wireless_paper_V1.1_FactoryTest.ino
+++ libraries/HeltecExample/examples/FactoryTest/Wireless_Paper_V1.1_FactoryTest/Wireless_paper_V1.1_FactoryTest.ino
@@ -27,11 +27,13 @@
 
 #include "LoRaWan_APP.h"
 #include <Wire.h>
-#include "HT_QYEG0213RWS800_BWR.h"
+#include "HT_lCMEN2R13EFC1.h"
 #include "images.h"
 
 // Initialize the display
-QYEG0213RWS800_BWR   factory_display(6, 5, 4,7, 3,2,-1, 6000000);//rst,dc,cs,busy,sck,mosi,miso,frequency
+HT_ICMEN2R13EFC1   factory_display(6, 5, 4, 7, 3, 2, -1, 6000000); //rst,dc,cs,busy,sck,mosi,miso,frequency
+uint8_t btn_t;
+uint8_t get_buff[255]={};
 /********************************* lora  *********************************************/
 #define RF_FREQUENCY                                470000000 // Hz
 
@@ -148,67 +150,102 @@ void lora_init(void)
 
 
 /********************************* lora  *********************************************/
-void logo(){
+void _4bit(uint8_t *buff){
+ byte *src = (byte*)buff;
+int i=0;
+  
+    for (int y = 0; y <= 120; y++) {
+      for ( int x = 0; x <= 250; x++) {
+        // Clip to display area
+        if ((x >= 0) && (y >= 0) && (x <= 250) && (y <= 122)) {
+          // Directly convert color to 4-bit gray scale
+        uint8_t  pix = 0;
+          pix |= ((*src++) >> 4) & 0x08;
+          pix |= ((*src++) >> 5) & 0x06;
+          pix |= ((*src++) >> 7);
+          pix ^= 0x0F;
+
+          i++;
+          /* Convert rgb color to gray scale
+          memcpy(rgb_color, src, 3);
+          src += 3;
+            gs_clr = (GS_FACT_R * rgb_color[0]) + (GS_FACT_G * rgb_color[1]) + (GS_FACT_B * rgb_color[2]);
+            if (gs_clr > 255) gs_clr = 255;
+            // Use only 4 bits & invert
+            //pix = ((uint8_t)gs_clr >> 4) ^ 0x0F;
+            pix = (uint8_t)gs_clr;
+
+            // Using gray scale lookup table
+            last_lvl = 0;
+            i = 0;
+            for (i=0; i<16; i++) {
+              if ((pix > last_lvl) && (pix <= lvl_buf_jpg[i])) {
+                pix = 15 - i;
+                  last_lvl = lvl_buf[i];
+                  break;
+                }
+                last_lvl = lvl_buf[i];
+            }
+          */
+            get_buff[i] = pix;
+//            gs_used_shades |= (1 << pix);
+        }
+        else src += 3; // skip
+      }
+    }
+  }
+ 
+void logo() {
   factory_display.clear();
   factory_display.update(BLACK_BUFFER);
 
-    factory_display.clear();
-      factory_display.drawXbm(35 ,25  , logo_width, logo_height, logo_bits);
+  factory_display.clear();
+  factory_display.drawXbm(90 , 50 , logo_width, logo_height, logo_bits);
   // factory_display.drawXbm(0,0,logo_width,logo_height,logo_bits);
-      factory_display.update(BLACK_BUFFER);
+  factory_display.update(BLACK_BUFFER);
 
   factory_display.display();
 }
 
 void WIFISetUp(void)
 {
+
+    
   // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.disconnect(true);
   delay(100);
   WiFi.mode(WIFI_STA);
   WiFi.setAutoConnect(true);
-  WiFi.begin("Heltec_3120","hunter3120");//fill in "Your WiFi SSID","Your Password"
+  WiFi.begin("TP-LINK_B8BC", "heltec_test"); //fill in "Your WiFi SSID","Your Password"
   delay(100);
 
   byte count = 0;
-  while(WiFi.status() != WL_CONNECTED && count < 10)
+  while (WiFi.status() != WL_CONNECTED && count < 10)
   {
     count ++;
     delay(500);
-
-    factory_display.clear();
-  factory_display.update(BLACK_BUFFER);
-    factory_display.clear();
-    factory_display.drawString(0, 0, "Connecting...");
-        factory_display.update(BLACK_BUFFER);
-
-  factory_display.display();
+    factory_display.display();
   }
-
   factory_display.clear();
-   factory_display.clear();
+  factory_display.clear();
   factory_display.update(BLACK_BUFFER);
-    factory_display.clear();
-  if(WiFi.status() == WL_CONNECTED)
+  factory_display.clear();
+  if (WiFi.status() == WL_CONNECTED)
   {
-   
-    factory_display.drawString(0, 0, "Connecting...OK.");
+    factory_display.drawString(0, 0, "Connecting...start");
+    factory_display.drawString(0, 20, "Connecting...OK.");
     factory_display.update(BLACK_BUFFER);
-
-  factory_display.display();//    delay(500);
+    factory_display.display();//    delay(500);
   }
   else
   {
     factory_display.clear();
-    factory_display.drawString(0, 0, "Connecting...Failed");
-        factory_display.update(BLACK_BUFFER);
-
+    factory_display.drawString(0, 0, "Connecting...start");
+    factory_display.drawString(0, 20, "Connecting...Failed");
+    factory_display.update(BLACK_BUFFER);
     factory_display.display();
     //while(1);
   }
-  factory_display.drawString(0, 10, "WIFI Setup done");
-      factory_display.update(BLACK_BUFFER);
-
   factory_display.display();
   delay(500);
 }
@@ -216,61 +253,44 @@ void WIFISetUp(void)
 void WIFIScan(unsigned int value)
 {
   unsigned int i;
-    WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA);
 
-  for(i=0;i<value;i++)
+  for (i = 0; i < value; i++)
   {
-     factory_display.clear();
-  factory_display.update(BLACK_BUFFER);
-    factory_display.clear();
-    factory_display.drawString(0, 20, "Scan start...");
-        factory_display.update(BLACK_BUFFER);
-
-    factory_display.display();
-
     int n = WiFi.scanNetworks();
-     factory_display.clear();
-  factory_display.update(BLACK_BUFFER);
-    factory_display.clear();
-    factory_display.drawString(0, 30, "Scan done");
-        factory_display.update(BLACK_BUFFER);
-
-    factory_display.display();
     delay(500);
-    factory_display.clear();
-
     if (n == 0)
     {
       factory_display.clear();
       factory_display.drawString(0, 0, "no network found");
-          factory_display.update(BLACK_BUFFER);
+      factory_display.update(BLACK_BUFFER);
 
       factory_display.display();
       //while(1);
     }
     else
-    {
+    { factory_display.clear();
       factory_display.drawString(0, 0, (String)n);
       factory_display.drawString(14, 0, "networks found:");
-          factory_display.update(BLACK_BUFFER);
+      factory_display.update(BLACK_BUFFER);
 
       factory_display.display();
       delay(500);
 
       for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-        factory_display.drawString(0, (i+1)*9,(String)(i + 1));
-        factory_display.drawString(6, (i+1)*9, ":");
-        factory_display.drawString(12,(i+1)*9, (String)(WiFi.SSID(i)));
-        factory_display.drawString(90,(i+1)*9, " (");
-        factory_display.drawString(98,(i+1)*9, (String)(WiFi.RSSI(i)));
-        factory_display.drawString(114,(i+1)*9, ")");
+        // Print SSID and RSSI for each network found
+        factory_display.drawString(0, (i + 1) * 9, (String)(i + 1));
+        factory_display.drawString(6, (i + 1) * 9, ":");
+        factory_display.drawString(12, (i + 1) * 9, (String)(WiFi.SSID(i)));
+        factory_display.drawString(90, (i + 1) * 9, " (");
+        factory_display.drawString(98, (i + 1) * 9, (String)(WiFi.RSSI(i)));
+        factory_display.drawString(114, (i + 1) * 9, ")");
 
         //factory_display.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
         delay(10);
       }
     }
-            factory_display.update(BLACK_BUFFER);
+    factory_display.update(BLACK_BUFFER);
 
     factory_display.display();
     delay(800);
@@ -291,19 +311,15 @@ void interrupt_handle(void)
   if (interrupt_flag)
   {
     interrupt_flag = false;
-    if (digitalRead(0) == 0)
-    {
-      if (rxNumber <= 2)
+          if(rxNumber <=3)
       {
-        resendflag = true;
+        resendflag=true;
       }
       else
       {
-        deepsleepflag = true;
+        deepsleepflag=true;
       }
-    }
   }
-
 }
 void VextON(void)
 {
@@ -316,19 +332,20 @@ void VextOFF(void) //Vext default OFF
   pinMode(45, OUTPUT);
   digitalWrite(45, HIGH);
 }
-void setup()
+uint8_t KEY_Scan(void)
 {
-  Serial.begin(115200);
+    static uint16_t keytimecount=0;    
+//    delay(1000);
 
-    VextON();
-  delay(100);
+       printf("display OK!!!\n");
+    if(btn_t==1)//如果长按时间大于1s则返回按键长按,在此处设置按键长按时长
+    {
+//    
+delay(100);
+    SPI.end();
+delay(100);
   factory_display.init();
-  factory_display.clear();
-              factory_display.update(BLACK_BUFFER);
-
-  factory_display.display();
-  logo();
-  delay(300);
+ delay(300);
   factory_display.clear();
 
   WIFISetUp();
@@ -339,9 +356,24 @@ void setup()
   WIFIScan(1);
 
 
-    logo();
+  logo();
+ 
 
-  lora_init();
+
+    } 
+  return 0;// 无按键按下
+}
+void setup()
+{
+  Serial.begin(115200);
+if(RF_FREQUENCY==470000000){ Serial.println("FREQUENCY :  LF");}
+if(RF_FREQUENCY==868000000){ Serial.println("FREQUENCY :  HF");}
+
+  VextON();
+
+ delay(100);
+    SPI.end();
+//  lora_init();
   chipid = ESP.getEfuseMac(); //The chip ID is essentially its MAC address(length: 6 bytes).
   Serial.printf("ESP32ChipID=%04X", (uint16_t)(chipid >> 32)); //print High 2 bytes
   Serial.printf("%08X\n", (uint32_t)chipid); //print Low 4bytes.
@@ -350,8 +382,9 @@ void setup()
 
   packet = "waiting lora data!";
 
-  delay(100);
+  
 
+  delay(100);
   pinMode(18 , OUTPUT);
   digitalWrite(18, LOW);
 }
@@ -359,7 +392,13 @@ void setup()
 
 void loop()
 {
-  interrupt_handle();
+   if (digitalRead(0) == 0){
+     btn_t++;
+     
+    KEY_Scan();
+    }
+   
+//  interrupt_handle();
   if (deepsleepflag)
   {
 
@@ -368,34 +407,34 @@ void loop()
     Radio.Sleep();
     SPI.end();
     Serial.printf("1");
-     pinMode(RADIO_DIO_1, ANALOG);
-      digitalWrite(RADIO_NSS, HIGH);
-     gpio_hold_en((gpio_num_t)RADIO_NSS);
-//  pinMode(RADIO_NSS, ANALOG);
-//    digitalWrite(RADIO_NSS, HIGH);
+    pinMode(RADIO_DIO_1, ANALOG);
+    digitalWrite(RADIO_NSS, HIGH);
+    gpio_hold_en((gpio_num_t)RADIO_NSS);
+    //  pinMode(RADIO_NSS, ANALOG);
+    //    digitalWrite(RADIO_NSS, HIGH);
 
-  pinMode(RADIO_RESET, ANALOG); 
-  pinMode(RADIO_BUSY, ANALOG);
-  pinMode(LORA_CLK, ANALOG);
-  pinMode(LORA_MISO, ANALOG);
-  pinMode(LORA_MOSI, ANALOG);
-//    pinMode(2, ANALOG);
-//    pinMode(3, ANALOG);
-//    pinMode(4, ANALOG);
-//    pinMode(5, ANALOG);
-//    pinMode(6, ANALOG);
-//    pinMode(7, ANALOG);
-//    pinMode(43, ANALOG);
-//    pinMode(44, ANALOG);
-//    pinMode(47, ANALOG);
-//    pinMode(48, ANALOG);
+    pinMode(RADIO_RESET, ANALOG);
+    pinMode(RADIO_BUSY, ANALOG);
+    pinMode(LORA_CLK, ANALOG);
+    pinMode(LORA_MISO, ANALOG);
+    pinMode(LORA_MOSI, ANALOG);
+    //    pinMode(2, ANALOG);
+    //    pinMode(3, ANALOG);
+    //    pinMode(4, ANALOG);
+    //    pinMode(5, ANALOG);
+    //    pinMode(6, ANALOG);
+    //    pinMode(7, ANALOG);
+    //    pinMode(43, ANALOG);
+    //    pinMode(44, ANALOG);
+    //    pinMode(47, ANALOG);
+    //    pinMode(48, ANALOG);
 
     esp_sleep_enable_timer_wakeup(600 * 1000 * (uint64_t)1000);
-  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
 
-//    esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,1);
+    //    esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,1);
     esp_deep_sleep_start();
- 
+
   }
 
   if (resendflag)
@@ -425,9 +464,12 @@ void loop()
     delay(10);
 
 
-    if ((rxNumber % 2) == 0)
+    if (( rxNumber% 5) == 0)
     {
       digitalWrite(18, HIGH);
+        deepsleepflag=true;
+
+      
     }
   }
   switch (state)
