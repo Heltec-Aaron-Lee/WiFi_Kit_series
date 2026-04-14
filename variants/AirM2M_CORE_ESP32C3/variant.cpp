9129619633e1047da6a955f7bfcfc339353df913
Quency-D <hj_zzns@163.com>
2026-04-14 18:32:32 +0800

    Remove all boards except heltec

--- variants/AirM2M_CORE_ESP32C3/variant.cpp
+++
@@ -1,9 +0,0 @@
-#include "Arduino.h"
-
-extern "C" void initVariant(void) {
-  // Stop LEDs floating
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  pinMode(LED_BUILTIN_AUX, OUTPUT);
-  digitalWrite(LED_BUILTIN_AUX, LOW);
-}
